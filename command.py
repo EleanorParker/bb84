487b191303d94fe7bdab357d04a2caf252c247ae
Luke Brown <lukelbro@gmail.com>
2020-02-21 16:29:41 +0000

    able to send a 4 step pulse

--- command.py
+++ command.py
@@ -1,27 +1,67 @@
-"""
-Program 4 voltage waveform
-"""
+'''
+Waveform Generator
+
+
+Todo:
+Perpare Device to send message at trigger point from laser
+'''
 
 import pyvisa
 import time
 
+"""
+EXPERIMENTAL PARAMETERS
+"""
+# voltages in V 
+VOL_1 = 0
+VOL_2 = 0.25
+VOL_3 = 0.5
+VOL_4 = 1
+
+BLOCK_SIZE = 4 # number of pulses in block
+PULSE_LENGTH = 1 # pulse lenght in secounds
+
+
+"""
+CONNECT TO DEVICE
+"""
 rm = pyvisa.ResourceManager()
-#print(rm.list_resources())
-my_instrument = rm.open_resource('USB0::0x0957::0x0407::MY44057486::INSTR')
-#my_instrument.write('OUTPut ON')
+print(rm.list_resources())
+awg = rm.open_resource('USB0::0x0957::0x0407::MY44057486::INSTR')
+awg.write('OUTPut ON')
+
+
+"""
+Prepare Block
+"""
 
 # dictionary of volatage values
 volages_def = {
-    "H": 0.1,
-    "s_plus": 0.2,
-    "V": 0.3,
-    "s_minus":0.4
+    "H": VOL_1,
+    "s_plus": VOL_2,
+    "V": VOL_3,
+    "s_minus": VOL_4
 }
 
+# message to send to device
+VOL_1 = VOL_1/5
+VOL_2 = VOL_2/5
+VOL_3 = VOL_3/5
+VOL_4 = VOL_4/5
 
-my_instrument.write('FREQ 100')
-my_instrument.write('APPLy:DC DEF, DEF, 0.5')
+message = 'DATA VOLATILE, ' + str(VOL_1)  + ', '  + str(VOL_2) + ', ' + str(VOL_3) + ', ' + str(VOL_4)
 
+# read message into device
+awg.write(message)
+awg.write('DATA:COPY STEPFUNC')
+awg.write('FUNC:USER STEPFUNC')
+
+# execute message
+awg.write('APPL:USER 10, 5 Vpp, 0') #frequency, amplitude, offset
+
+
+# For pulse sweep 
+'''
 lower = 0
 upper = 5
 length = 1000
@@ -30,5 +70,7 @@ volts = [lower + x*(upper-lower)/(length-1) for x in range(length)]
 for i in range(len(volts)):
     message = 'APPLy:DC DEF, DEF, '
     message += str(volts[i])
-    my_instrument.write(message)
-    time.sleep(0.5)
+    awg.write(message)
+    time.sleep(0.1)
+    print(volts[i])
+'''
