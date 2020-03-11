95fde984d6cc80a886d9005d897c2a67d097a819
Luke Brown <lukelbro@gmail.com>
2020-03-11 15:10:37 +0000

    Histogram plotting and data processing

--- awg_sweep.py
+++ awg_sweep.py
@@ -1,5 +1,20 @@
 '''
+Script to voltage sweep the AOM, so to identify the associated phases.
+Data is read using a data Acquisition board.
 
+Output:
+    - Graph of voltage sweep and 
+    - Data of voltage sweep
+    - The data is saved to sweep_data/todays-date/
+
+Parameters:
+    SWEEP_NAME: Name for filename
+    FOLDER_NAME: Target directory for saved files
+    Voltage_MIN: Start voltage of sweep
+    VOLTAGE_MAX: End voltage of sweep
+    SWEEP_LENGTH: Number of data points
+    AMPLIFICATION: Included in filenames, should be set manually not
+                   used as input in script 
 '''
 
 import pyvisa
@@ -7,8 +22,21 @@ import time
 import nidaqmx
 import matplotlib.pyplot as plt
 import csv
+from datetime import date
+import os
 
-SWEEP_NAME = 'laser_1 10^-6'
+"""
+PARAMETERS
+"""
+# Parameters for book keeping
+SWEEP_NAME = 'detector_4'
+FOLDER_NAME = 'sweep_data/' +  str(date.today()) + "/"
+AMPLIFICATION = 7
+
+# Parameters used in script
+VOLTAGE_MAX = 0.75
+VOLTAGE_MIN = -0.75
+SWEEP_LENGTH = 1000
 
 """
 CONNECT TO AWG
@@ -26,7 +54,7 @@ CONNECT TO DAQ
 Connects to the DAQ device using nidaqmx
 """
 daq = nidaqmx.Task()
-daq.ai_channels.add_ai_voltage_chan("Dev1/ai1", min_val=0, max_val=5)
+daq.ai_channels.add_ai_voltage_chan("Dev1/ai1", min_val=0, max_val=10)
 print("Connected to DAQ device ID: " + str(daq.devices))
 
 
@@ -36,9 +64,9 @@ PREPARE SWEEP
 Makes array of voltages, lower is the starting voltage, upper is the
 ending voltage, length is the number of steps.
 """
-lower = -0.8
-upper = 0.8
-length = 1000
+lower = VOLTAGE_MIN
+upper = VOLTAGE_MAX
+length = SWEEP_LENGTH
 volts = [lower + x*(upper-lower)/(length-1) for x in range(length)]
 
 
@@ -50,24 +78,29 @@ for i in range(len(volts)):
     message = 'APPLy:DC DEF, DEF, '
     message += str(volts[i])
     awg.write(message)
-    time.sleep(0.03)
+    time.sleep(0.18)
     power.append(daq.read())
     print("vol: " + str(volts[i]) + " pow: " + str(power[i]))
 
 """
 MAKE PLOTS AND SAVE DATA
 """
+# Make directory
+if not os.path.exists(FOLDER_NAME):
+    os.mkdir(FOLDER_NAME)
+    print("Directory " , FOLDER_NAME ,  " Created ")
+else:    
+    print("Directory " , FOLDER_NAME ,  " already exists")
 
+#power = [pow(AMPLIFICATION, -1)*x for x in power]
 plt.figure()
 plt.plot(volts, power)
 plt.xlabel("Voltage")
 plt.ylabel("Power")
-plt.savefig('sweep_' + SWEEP_NAME + '.pdf')
-plt.savefig('sweep_' + SWEEP_NAME + '.png', dpi=1200)
+plt.savefig(FOLDER_NAME + 'sweep_' + SWEEP_NAME + '_' + str(AMPLIFICATION) + '.pdf')
+plt.savefig(FOLDER_NAME + 'sweep_' + SWEEP_NAME + '_' + str(AMPLIFICATION) + '.png', dpi=1200)
 
-
-
-with open('sweep_data_' + SWEEP_NAME + '.csv', 'w') as f:
+with open(FOLDER_NAME + 'sweep_data_' + SWEEP_NAME + '_' + str(AMPLIFICATION) + '.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerows(zip(volts, power))
 
@@ -77,6 +110,5 @@ DISCONNECT DEVICES
 
 The DAQ throws an error if not disconnected.
 """
-
 daq.close()
 plt.show()
--- overlay_plot.py
+++ overlay_plot.py
@@ -1,39 +1,120 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-
-# Detector 1
-df = pd.read_csv('sweep_data_laser_1_10^-6.csv', sep=',', header=None ,
-                 names = ['voltage', 'power'])
-voltage_1 = df['voltage']
-power_1 = df['power']
-
-# Detector 2
-df = pd.read_csv('sweep_data_laser_2 4.csv', sep=',', header=None ,
-                 names = ['voltage', 'power'])
-voltage_2 = df['voltage']
-power_2 = df['power']
-
-# Detector 3
-df = pd.read_csv('sweep_data_laser_3_1.csv', sep=',', header=None ,
-                 names = ['voltage', 'power'])
-voltage_3 = df['voltage']
-power_3 = df['power']
-
-# Detector 4
-df = pd.read_csv('sweep_data_laser_4_1.csv', sep=',', header=None ,
-                 names = ['voltage', 'power'])
-voltage_4 = df['voltage']
-power_4 = df['power']
-
-# Plot all Detector Powers as a fucntion of EOM Voltage Sweeps
-plt.figure()
-plt.plot(voltage_1, power_1, 'b-', label='Detector 1')
-plt.plot(voltage_2, power_2, 'r-', label='Detector 2')
-plt.plot(voltage_3, power_3, 'g-', label='Detector 3')
-plt.plot(voltage_4, power_4, 'y-', label='Detector 4')
-plt.legend()
+from matplotlib.ticker import MaxNLocator
 
 def voltage_of_max_power(voltage, power):
     max_idx = np.argmax(power)
     return voltage[max_idx]
+
+def normalise(power):
+    max_idx = np.argmax(power)
+    N = power[max_idx]
+    return power/N
+
+def get_index(value, df):
+    index = abs(df - value).idxmin()
+    return index
+
+if __name__ == "__main__":
+    
+    detector_1 = 'sweep_data_detector 1_7.csv'
+    detector_2 = 'sweep_data_detector 2_7.csv'
+    detector_3 = 'sweep_data_detector 3_7.csv'
+    detector_4 = 'sweep_data_detector 4_7.csv'
+    path = 'sweep_data/2020-03-09/'
+
+    detector_1 = path + detector_1
+    detector_2 = path + detector_2
+    detector_3 = path + detector_3
+    detector_4 = path + detector_4
+
+    # Detector 1
+    df = pd.read_csv(detector_1, sep=',', header=None ,
+                     names = ['voltage', 'power'])
+    voltage_1 = df['voltage']
+    power_1 = df['power']
+    
+    # Detector 2
+    df = pd.read_csv(detector_2, sep=',', header=None ,
+                     names = ['voltage', 'power'])
+    voltage_2 = df['voltage']
+    power_2 = df['power']
+    
+    # Detector 3
+    df = pd.read_csv(detector_3, sep=',', header=None ,
+                     names = ['voltage', 'power'])
+    voltage_3 = df['voltage']
+    power_3 = df['power']
+    
+    # Detector 4
+    df = pd.read_csv(detector_4, sep=',', header=None ,
+                     names = ['voltage', 'power'])
+    voltage_4 = df['voltage']
+    power_4 = df['power']
+    
+    # Plot all Detector Powers as a fucntion of EOM Voltage Sweeps
+    plt.figure()
+    plt.plot(voltage_1, power_1, 'tab:blue', label='Detector 1')
+    plt.plot(voltage_2, power_2, 'tab:red', label='Detector 2')
+    plt.plot(voltage_3, power_3, 'tab:green', label='Detector 3')
+    plt.plot(voltage_4, power_4, 'tab:purple', label='Detector 4')
+    plt.legend()
+    
+    power_1_norm = normalise(power_1)
+    power_2_norm = normalise(power_2)
+    power_3_norm = normalise(power_3)
+    power_4_norm = normalise(power_4)
+    
+    # Plot all Detector Powers as a function of EOM Voltage Sweeps
+    plt.figure()
+    plt.plot(voltage_1, power_1_norm, 'tab:blue', label='Detector 1')
+    plt.plot(voltage_2, power_2_norm, 'tab:red', label='Detector 2')
+    plt.plot(voltage_3, power_3_norm, 'tab:green', label='Detector 3')
+    plt.plot(voltage_4, power_4_norm, 'tab:purple', label='Detector 4')
+    plt.legend()
+    
+    print('Detector 1 : Voltage at Max Power = %.3f V' % voltage_of_max_power(voltage_1, power_1))
+    print('Detector 2 : Voltage at Max Power = %.3f V' % voltage_of_max_power(voltage_2, power_2))
+    print('Detector 3 : Voltage at Max Power = %.3f V' % voltage_of_max_power(voltage_3, power_3))
+    print('Detector 4 : Voltage at Max Power = %.3f V' % voltage_of_max_power(voltage_4, power_4))
+
+    
+    # Plot Estimated historgrams
+    
+    max_voltage = [voltage_of_max_power(voltage_1, power_1),
+                voltage_of_max_power(voltage_2, power_2),
+                voltage_of_max_power(voltage_3, power_3),
+                voltage_of_max_power(voltage_4, power_4)]
+    
+    for voltage in max_voltage:
+        v1_index = get_index(voltage, voltage_1)
+        v2_index = get_index(voltage, voltage_2)
+        v3_index = get_index(voltage, voltage_3)
+        v4_index = get_index(voltage, voltage_4)
+        
+        probability = np.array([power_1[v1_index],
+                       power_2[v2_index],
+                       power_3[v3_index],
+                       power_4[v4_index]])
+        plt.figure()
+        plt.title("Voltage: " + str(voltage) + "$\mu$ V")    
+        plt.bar(range(1,5), probability, color=('tab:blue', 'tab:red', 'tab:green', 'tab:purple'))
+        plt.xlabel("Detector")
+        plt.ylabel("Voltage")
+        plt.xticks(range(1,5))
+
+        # make normalised version
+        probability = pow(np.amax(probability), - 1) * probability
+        plt.figure()
+        plt.title("Voltage: " + str(voltage) + "$\mu$ V")    
+        plt.bar(range(1,5), probability, color=('tab:blue', 'tab:red', 'tab:green', 'tab:purple'))
+        plt.xlabel("Detector")
+        plt.ylabel("Probability")
+        plt.xticks(range(1,5))
+        print("%.3f" % voltage   + " Disturbance : %.3f" % np.amin(probability))
+        
+plt.show()
+               
+
+    
